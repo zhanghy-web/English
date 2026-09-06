dcf0e6bab97460c2b6653a293e7b1e9cfd60f48d
zhanghy-web <cnsdzl@gmail.com>
2026-09-06 12:21:11 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* 高中英语学习工具 - 离线缓存 Service Worker */
+var CACHE = 'eng-tool-v1';
+var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) { return c.addAll(FILES); }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k !== CACHE) return caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      return hit || fetch(e.request).then(function (resp) {
+        var copy = resp.clone();
+        caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        return resp;
+      }).catch(function () {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
